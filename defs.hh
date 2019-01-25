f44cf3edb085d47e18dc1ba249d5960b5952eaa7
Paul Maanen <paulmaanen@openMHA.org>
2019-01-25 15:32:25 +0100

    Add version info header

---
+++ defs.hh
@@ -0,0 +1,18 @@
+/*****************************************************************************/
+/*                                                                           */
+/*   version control                                                         */
+/*                                                                           */
+/*****************************************************************************/
+
+/** Major version number of JACK_PLAYREC */
+#define JACK_PLAYREC_VERSION_MAJOR 1
+
+/** Minor version number of JACK_PLAYREC */
+#define JACK_PLAYREC_VERSION_MINOR 0
+
+/** Release number of JACK_PLAYREC */
+#define JACK_PLAYREC_VERSION_RELEASE 0
+
+/** Build number of JACK_PLAYREC (currently unused) */
+#define JACK_PLAYREC_VERSION_BUILD 0
+
