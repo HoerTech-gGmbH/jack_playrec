0c291e398af83239f0860241113bdab8fe4b454c
Paul Maanen <paulmaanen@openMHA.org>
2021-02-17 14:14:31 +0100

    Amend jack_playrec default installation directory to PATH if not found in default PATH
    Summary:
    On some systems the jack_playrec installation path is not in the default PATH.
    This commit adds the directory to the PATH at runtime.
    Closes T1266
    
    Test Plan:
    * Use installers to install jack_playrec
    * Add jack_playrec.m to Matlab/Octave path
    * Execute `jack_playrec` in Octave/Matlab
    
    Reviewers: giso, tobias, hendrikk
    
    Subscribers: #jack_playrec
    
    Maniphest Tasks: T1266
    
    Differential Revision: https://dev.openmha.org/D537

---
+++ set_environment.m
@@ -0,0 +1,53 @@
+% This file is part of jack_playrec
+% Copyright (C) 2021 Hörtech gGmbH
+%
+%  This program is free software: you can redistribute it and/or modify
+%  it under the terms of the GNU General Public License as published by
+%  the Free Software Foundation, either version 3 of the License, or
+%  (at your option) any later version.
+%
+%  This program is distributed in the hope that it will be useful,
+%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+
+function set_environment()
+% set_environment()
+% Checks if jack_playrec can be found in the PATH
+% And amends the PATH with the operating system's
+% default jack_playrec installation directory
+% if it is not found. If jack_playrec can not be
+% found after amendment, exits with an error message
+  if(check_playrec()==0)
+    return
+  end
+
+  if ismac % Macos
+    bin='/usr/local/bin';
+    delim=':';
+  elseif ispc % Windows
+    bin='C:\Program Files\jack_playrec\bin';
+    delim=';';
+  else % Linux
+    bin='/usr/bin';
+    delim=':';
+  end
+  curPATH=getenv('PATH');
+  if(isempty(strfind(curPATH,bin)))
+    setenv('PATH',[getenv('PATH') delim bin])
+  end
+  if(check_playrec()~=0)
+    error('jack_playrec was found in PATH and/or default installation directory!'...
+            ' Please add the location of jack_playrec to the PATH!');
+end
+
+function res = check_playrec()
+% res=check_playrec()
+% Checks if jack_playrec can be found in the PATH
+% res: 0 if jack_playrec could be found, non-zero otherwise
+  if ispc
+    [res,a]=system('where jack_playrec');
+  else
+    [res,a]=system('which jack_playrec');
+  end
+end
